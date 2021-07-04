678ec480a09e72e016306e4237bf59e5f1a0c447
Cheng <machengiam@gmail.com>
2021-07-05 01:14:11 +1000

    actix init

---
+++ backend/src/db.rs
@@ -0,0 +1,106 @@
+use crate::utils;
+use sqlx::sqlite::{SqliteConnectOptions, SqlitePool, SqliteRow};
+use sqlx::{ConnectOptions, Connection, FromRow};
+use std::path::PathBuf;
+use tokio::fs;
+
+pub async fn get_db_conn() -> SqlitePool {
+    let dir = utils::get_config_dir();
+    let db_file = dir.join("main.db");
+    if !db_file.as_path().exists() {
+        if let Err(e) = create_db(&db_file).await {
+            panic!("Cannot create database\n {}", e);
+        }
+    }
+
+    match get_conn_pool(&db_file).await {
+        Ok(pool) => pool,
+        Err(e) => panic!("Cannot get database connection pool\n {}", e),
+    }
+}
+
+async fn create_db(db_file: &PathBuf) -> anyhow::Result<()> {
+    let prefix = db_file
+        .parent()
+        .expect("Cannot retrieve the parent dir of db file");
+    fs::create_dir_all(&prefix).await?;
+
+    let mut conn = SqliteConnectOptions::new()
+        .filename(db_file)
+        .create_if_missing(true)
+        //.log_statements(log::LevelFilter::Debug)
+        .connect()
+        .await?;
+
+    let sql_init_file =
+        std::env::var("INIT_SQLITE_FILE").expect("Cannot get init SQL file from env");
+    let sql = fs::read_to_string(sql_init_file).await?;
+    sqlx::query(&sql).execute(&mut conn).await?;
+    debug!("Database created at {:?}", db_file);
+    conn.close();
+
+    Ok(())
+}
+
+async fn get_conn_pool(db_file: &PathBuf) -> anyhow::Result<SqlitePool> {
+    let db_file_str = db_file.to_str().expect("Cannot parse database filename");
+    let option = SqliteConnectOptions::new().filename(db_file_str);
+    //option.log_statements(log::LevelFilter::Trace);
+    let pool = SqlitePool::connect_with(option).await?;
+
+    Ok(pool)
+}
+
+pub async fn fetch_single<T>(sql: &str, args: Vec<String>, pool: &SqlitePool) -> anyhow::Result<T>
+where
+    T: Send + Unpin + for<'a> FromRow<'a, SqliteRow>,
+{
+    let stmt = prepare_sql(sql, args);
+    Ok(stmt.fetch_one(pool).await?)
+}
+
+pub async fn fetch_multiple<T>(
+    sql: &str,
+    args: Vec<String>,
+    pool: &SqlitePool,
+) -> anyhow::Result<Vec<T>>
+where
+    T: Send + Unpin + for<'a> FromRow<'a, SqliteRow>,
+{
+    let stmt = prepare_sql(sql, args);
+    Ok(stmt.fetch_all(pool).await?)
+}
+
+pub async fn execute(sql: &str, args: Vec<String>, pool: &SqlitePool) -> anyhow::Result<()> {
+    let stmt = prepare_exec_sql(sql, args);
+    stmt.execute(pool).await?;
+
+    Ok(())
+}
+
+fn prepare_sql<T>(
+    sql: &str,
+    args: Vec<String>,
+) -> sqlx::query::QueryAs<'_, sqlx::Sqlite, T, sqlx::sqlite::SqliteArguments<'_>>
+where
+    T: Send + Unpin + for<'a> FromRow<'a, SqliteRow>,
+{
+    let mut stmt = sqlx::query_as(sql);
+    for arg in args.iter() {
+        stmt = stmt.bind(arg.to_owned());
+    }
+
+    stmt
+}
+
+fn prepare_exec_sql(
+    sql: &str,
+    args: Vec<String>,
+) -> sqlx::query::Query<'_, sqlx::Sqlite, sqlx::sqlite::SqliteArguments<'_>> {
+    let mut stmt = sqlx::query(sql);
+    for arg in args.iter() {
+        stmt = stmt.bind(arg.to_owned());
+    }
+
+    stmt
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,35 +1,84 @@
+mod db;
+mod entity;
+mod utils;
+use actix_files::{NamedFile, self};
 use actix_web::dev::Server;
-use actix_web::{rt::System, web, App, HttpResponse, HttpServer};
+use actix_web::{rt::System, get, web, App, HttpResponse, HttpServer, Result};
 use std::sync::mpsc::{self, Sender};
 use std::{io, thread};
+use std::sync::Mutex;
+use sqlx::sqlite:: SqlitePool;
+use sqlx::{Pool, Sqlite};
+use tokio::runtime::Runtime;
+
+#[macro_use]
+extern crate log;
+
+struct AppState {
+    first_run: Mutex<bool>,
+    pool: Mutex<SqlitePool>
+}
 
 #[actix_web::main]
 async fn main() {
-    let (tx, rx) = mpsc::channel();
-    thread::spawn(move || run_sys(tx));
-    let server = rx.recv().unwrap();
+    dotenv::dotenv().ok();
+    env_logger::Builder::from_env("LOG_LEVEL").init();
 
-    println!("Running...");
-    thread::sleep(std::time::Duration::from_secs(5));
-    println!("stopping server");
-    server.stop(true).await;
-    println!("stopped");
-    thread::sleep(std::time::Duration::from_secs(10));
+    let (tx, rx) = mpsc::channel();
+    let handle = thread::spawn(move || init_server(tx));
+
+    // let server = rx.recv().unwrap();
+    // thread::sleep(std::time::Duration::from_secs(5));
+    // server.stop(true).await;
+
+    if let Err(e) =  handle.join() {
+        println!("Got error in thread joining: {:?}", e);
+    }
 }
 
-fn run_sys(tx: Sender<Server>) -> io::Result<()> {
-    let sys = System::new("http-server");
+fn init_server(tx: Sender<Server>) -> io::Result<()> {
+    let system = System::new("http-server");
+    let rt  = Runtime::new().unwrap();
+    let state = web::Data::new(rt.block_on(init_app_state()));
+    let react_dir = std::env::var("REACT_DIR").expect("Cannot get frontend dir from env");
 
-    let srv = HttpServer::new(|| {
-        App::new().route(
-            "/",
-            web::get().to(|| HttpResponse::Ok().body("Hello world")),
-        )
+    let server = HttpServer::new(move || {
+        App::new().app_data(state.clone())
+            .service(index)
+            .service(actix_files::Files::new("/", react_dir.clone()).index_file("index.html"))
     })
     .bind("127.0.0.1:8080")?
     .shutdown_timeout(3)
     .run();
 
-    let _ = tx.send(srv);
-    sys.run()
+    let _ = tx.send(server);
+    system.run()
+}
+
+async fn init_app_state() -> AppState {
+    let pool = db::get_db_conn().await;
+    let first_run = read_site(&pool).await.first_run == 1;
+
+    AppState{first_run: Mutex::new(first_run), pool: Mutex::new(pool)}
+}
+
+async fn read_site(pool: &Pool<Sqlite>) -> entity::Site {
+    let sql = "SELECT * FROM site";
+    let args = vec![];
+
+    match db::fetch_single::<entity::Site>(sql, args, pool).await {
+        Ok(site) => site,
+        Err(e) => panic!("Cannot read configuration: {}", e),
+    }
+}
+
+#[get("/")]
+async fn index(state: web::Data<AppState>) -> HttpResponse {
+    let first_run = state.first_run.lock().unwrap();
+    if *first_run {
+        HttpResponse::TemporaryRedirect().header("Location", "/setup/index.html").finish()
+    } else {
+        // HttpResponse::TemporaryRedirect().header("Location", "/index.html").finish()
+        HttpResponse::Ok().finish()
+    }
 }
