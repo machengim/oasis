e89f871a6fe05a1a671e0f85fa11d9c1139adc35
Cheng <machengiam@gmail.com>
2021-07-17 18:34:45 +1000

    range response

--- backend/src/api/mod.rs
+++ backend/src/api/mod.rs
@@ -1,9 +1,11 @@
+pub mod file;
 pub mod setup;
 pub mod sys;
 use rocket::Route;
 
 pub fn serve_api() -> Vec<Route> {
     let mut apis = vec![];
+    apis.append(&mut file::route());
     apis.append(&mut sys::route());
     apis.append(&mut setup::route());
 
--- backend/src/entity/mod.rs
+++ backend/src/entity/mod.rs
@@ -1,3 +1,4 @@
 pub mod auth;
+pub mod range;
 pub mod request;
 pub mod site;
